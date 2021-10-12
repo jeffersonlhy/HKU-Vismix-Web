370a8fb2af7c28b0d88687a219be18d4d9e7e402
Jeff <jeffersonlhy@gmail.com>
2021-10-13 00:30:42 +0800

    update date

--- src/index/components/Footer.js
+++ src/index/components/Footer.js
@@ -4,7 +4,7 @@ import "./App.css";
 function Footer() {
 	return (
 		<footer class="w-full pt-10 px-12 pb-12 lg:pt-14 lg:pb-20 lg:px-20 bg-row-grey text-sub-font-grey text-xxs lg:text-ssm leading-5 lg:leading-7">
-			<p>Course information correct as at 2021/10/13 12:17.</p>
+			<p>Course information correct as at 2021/10/13 12:27.</p>
 			<p>Designed and built by the Vismix Core Team.</p>
 			<p>Copyright © 2021 - Present, Vismix. All rights reserved.</p>
 		</footer>
